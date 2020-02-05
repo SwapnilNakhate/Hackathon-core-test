6a58c423b775b5033652cf31c371a2486f49b5ea
Amaresh Kumar <amaresh.kumar@BANCTECINDIA.IN>
2020-02-05 11:19:03 +0530

    deployment yaml

--- src/dataaccess/dataAccess.ts
+++ src/dataaccess/dataAccess.ts
@@ -17,12 +17,14 @@ class DataAccess {
             console.log("Connected to mongodb.");
         });
         Mongoose.set("debug", true);
-        let dbURL;// = "mongodb://sbr-devp-129:27017/" + dbConfig;
+        let dbURL;
         if(process.env.pathToMongoDb != null) {
             
             dbURL = process.env.pathToMongoDb;
         } else {
-            dbURL = "mongodb://localhost:27017/" + dbConfig;
+            // dbURL = "mongodb://localhost:27017/" + dbConfig;
+            // dbURL = "mongodb://sbr-devp-129:27017/" + dbConfig;
+            dbURL = "mongodb://amaresh15:Ami1982#@qadbcluster-shard-00-00-1hsyg.azure.mongodb.net:27017,qadbcluster-shard-00-01-1hsyg.azure.mongodb.net:27017,qadbcluster-shard-00-02-1hsyg.azure.mongodb.net:27017/test?ssl=true&replicaSet=QADBcluster-shard-0&authSource=admin&retryWrites=true&w=majority";
         }
         this.mongooseInstance = Mongoose.connect(dbURL, { useNewUrlParser: true });
         return this.mongooseInstance;
